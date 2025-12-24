e1c19dae3bee5ffa662eaf1dd874c5f267cf21d5
Bede Abbe <bede.abbe@gmail.com>
2025-12-24 12:29:48 +0000

    changes

--- models/customers.go
+++ models/customers.go
@@ -12,27 +12,30 @@ import (
 )
 
 type Customers struct {
-	CustomerId           int64                `orm:"auto"`
-	CustomerNumber       string               `orm:"column(customer_number);size(255)"`
-	FullName             string               `orm:"column(full_name);size(255)"`
-	ImagePath            string               `orm:"column(image_path);size(255)"`
-	Email                string               `orm:"column(email);size(255);null"`
-	PhoneNumber          string               `orm:"column(phone_number);size(255);null"`
-	Gender               string               `orm:"column(gender);size(10);null"`
-	Location             string               `orm:"column(location);size(255);null"`
-	IdentificationNumber string               `orm:"column(identification_number);size(255);null"`
-	Branch               *Branches            `orm:"rel(fk);column(branch);omitempty;null"`
-	Shop                 *Shops               `orm:"rel(fk);omitempty;null"`
-	CustomerCategory     *Customer_categories `orm:"rel(fk);omitempty;null"`
-	Nickname             string               `orm:"size(100);omitempty;null"`
-	Dob                  time.Time            `orm:"column(dob);type(datetime)"`
-	DateCreated          time.Time            `orm:"type(datetime)"`
-	DateModified         time.Time            `orm:"type(datetime)"`
+	CustomerId           int64                 `orm:"auto"`
+	CustomerNumber       string                `orm:"column(customer_number);size(255)"`
+	FullName             string                `orm:"column(full_name);size(255)"`
+	ImagePath            string                `orm:"column(image_path);size(255)"`
+	Email                string                `orm:"column(email);size(255);null"`
+	PhoneNumber          string                `orm:"column(phone_number);size(255);null"`
+	Gender               string                `orm:"column(gender);size(10);null"`
+	Location             string                `orm:"column(location);size(255);null"`
+	IdentificationType   *Identification_types `orm:"rel(fk);column(identification_type_id);omitempty;null"`
+	IdentificationNumber string                `orm:"column(identification_number);size(255);null"`
+	Branch               *Branches             `orm:"rel(fk);column(branch);omitempty;null"`
+	Shop                 *Shops                `orm:"rel(fk);omitempty;null"`
+	CustomerCategory     *Customer_categories  `orm:"rel(fk);omitempty;null"`
+	Nickname             string                `orm:"size(100);omitempty;null"`
+	Dob                  time.Time             `orm:"column(dob);type(datetime)"`
+	DateCreated          time.Time             `orm:"type(datetime)"`
+	DateModified         time.Time             `orm:"type(datetime)"`
 	CreatedBy            int
 	ModifiedBy           int
 	Active               int
-	User                 *Users    `orm:"rel(fk);omitempty;null"`
-	LastTxnDate          time.Time `orm:"type(datetime)"`
+	User                 *Users                         `orm:"rel(fk);omitempty;null"`
+	LastTxnDate          time.Time                      `orm:"type(datetime)"`
+	EmergencyContacts    []*Customer_emergency_contacts `orm:"reverse(many);null;"`
+	Guarantors           []*Customer_guarantors         `orm:"reverse(many);null;"`
 }
 
 func init() {
